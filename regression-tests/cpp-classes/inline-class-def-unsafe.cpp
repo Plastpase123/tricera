0a1310833b7d95c7d703fff059c33d0d1eb81d50
plastpase123 <linus.hellstrm@hotmail.com>
2026-04-21 17:03:09 +0200

    Updated regression tests

--- regression-tests/cpp-classes/inline-class-def-unsafe.cpp
+++ regression-tests/cpp-classes/inline-class-def-unsafe.cpp
@@ -13,15 +13,11 @@ int main() {
       void set(int val) {
         x = val;
       }
-      int get() {
-        return x;
-      }
   } l;
 
   l.set(1);
   g.set(2);
 
-  assert(l.x == 1 && g.x == 2);
-  assert(l.get() == 1);
+  assert(l.x == 2 && g.x == 5);
   return 0;
 }
